31d8dbb2b9ac87ead5ed249b0902f03018a55541
Cameron Jenner <cjenner37@gmail.com>
2016-07-11 11:20:03 -0600

    updated

--- cipher.rb
+++ cipher.rb
@@ -1,27 +1,15 @@
-puts "Enter a message"
-a = gets.chomp
-
 def caesar_cipher(message, key)
-	alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u"]
-	message.downcase.split(//).map do |i|
-		cipher = []
-			cipher << alphabet.index(i)
-			cipher.map! do |e| 
-				alphabet[e + key]
-			end
-		print cipher.join.to_s
+	letters = ('a'..'z').to_a.concat(('A'..'Z').to_a)
+	code = ""
+
+	message.each_char do |i|
+		if letters.include?(i)
+			code += letters[letters.index(i) - key]
+		else 
+			code += i 
+		end
 	end
+	print code
 end
 
-
-caesar_cipher(a, 5)
-
-# var alphabet
-# define caesar_cipher(string, key)
-# ciphered_message = ""
-# split string
-# find each string character in alphabet
-	# get index
-	# index + key to get new_character from alphabet
-	# ciphered_message << new_character
-# puts ciphered_message
+caesar_cipher("AaZz", 1)
